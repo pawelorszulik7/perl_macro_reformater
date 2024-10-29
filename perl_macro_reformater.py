0ffa33b3eeebf1bb0510dc9244b3c0b7147a960c
Orszulik, Pawel <pawel.orszulik@intel.com>
2024-10-29 06:25:45 -0700

    Add simple syntax checker

--- perl_macro_reformater.py
+++ perl_macro_reformater.py
@@ -7,6 +7,7 @@ class PerlMacroReformater:
         self.file_content = []
 
     def read_file(self):
+        print(f"INFO: Reading file {self.file_path}")
         lines = []
         try:
             with open(self.file_path, 'r') as f:
@@ -22,13 +23,17 @@ class PerlMacroReformater:
             fixed_file_path = self.file_path.with_stem(self.file_path.stem + '_reformated')
             with open(fixed_file_path, 'w') as f:
                 f.write(''.join(self.file_content))
+            print(f"INFO: Changes saved to file {fixed_file_path}")
         except Exception as e:
             print(f"Error: Can't save file {fixed_file_path}: {e}")
 
     def run_reformater(self):
+        print("")
+        print(49 * "=" + "Perl Macro Reformater" + 50 * "=")
         self.file_content = self.read_file()  
         self.file_content = self.fix_indentation()
         self.save_file()
+        print(120 * "=")
          
     def fix_indentation(self):
         if not self.file_content:
@@ -44,22 +49,42 @@ class PerlMacroReformater:
         current_indentation = 0
         indent = '    '
 
+        # Simple syntax checker
+        if_foreach_count = 0
+        end_count = 0 
+
         for line in file_lines_reversed:
             if re.search(r'\[%\s*?END\s*?-%\]', line):
                 line = current_indentation * indent + line 
                 current_indentation += 1
+                end_count += 1
             elif re.search(r'\[%.*?(IF|FOREACH).*?-%\]', line):
-                current_indentation -= 1
+                if current_indentation > 0:
+                    current_indentation -= 1
                 line = current_indentation * indent + line 
+                if_foreach_count += 1
+            elif re.search(r'\[%\s*?ELSE\s*?-%\]', line):
+                line = (current_indentation - 1) * indent + line                 
             else:
                 line = current_indentation * indent + line   
 
             fixed_file_lines_reversed.insert(0, line) # insert at the beginning
         
+        # Simple syntax checker
+        if if_foreach_count > end_count:
+            print(f"WARNING: Missing [% END -%] closing IF/FOREACH statement detected in file {self.file_path}")
+            print(f"WARNING: Due to syntax errors indentation is fixed only partially in file {self.file_path}")
+        elif if_foreach_count < end_count:
+            print(f"WARNING: Unnecessary [% END -%] detected in file {self.file_path}")
+            print(f"WARNING: Due to syntax errors indentation is fixed only partially in file {self.file_path}")
+        else:
+            print(f"INFO: Fixed indentation in file {self.file_path}")
+
         return fixed_file_lines_reversed
 
 
 if __name__ == '__main__':
     file_path = '/nfs/site/disks/zsc11_avs_00049/porszuli/ace-master/tools/collage/assemble/templates/adhoc_connection.txt.siphdas'
+    # file_path = '/nfs/site/disks/zsc11_avs_00049/porszuli/ace-master/tools/collage/assemble/templates/par.txt.siphdas'
     reformater = PerlMacroReformater(file_path)
     reformater.run_reformater()
